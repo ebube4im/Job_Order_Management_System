d7240f3727950d9bfc63bf69cb15568d618d035b
EbubezimDev <Canyanwaokoro@chamsswitch.com>
2024-10-07 14:55:28 +0100

    Add project files.

---
+++ Job_Order_Management_System/Program.cs
@@ -0,0 +1,15 @@
+namespace Job_Order_Management_System
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+            var app = builder.Build();
+
+            app.MapGet("/", () => "Hello World!");
+
+            app.Run();
+        }
+    }
+}
